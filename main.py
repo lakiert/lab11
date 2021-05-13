d0ac2bff1ad208662bb80863a60e85b8654d7a40
lakiert <aleksandra.lakiert@gmail.com>
2021-05-13 14:03:53 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from math import *
+
+# zad1
+
+# fig = plt.figure()
+# ax = fig.gca(projection='3d')
+# 
+#
+# z = np.linspace(0, 2 * np.pi)
+# x = np.sin(z)
+# y = 2 * np.cos(z)
+#
+# ax.plot(z, x, y)
+# plt.show()
